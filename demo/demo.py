c86133c32b77e8555d94c6534b4685292a4a2ef9
antoine491000 <antoine.colas@isen-ouest.yncrea.fr>
2025-09-09 14:34:00 +0200

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,5 +1,17 @@
 import subprocess
+import os
 
+out_dir = "data"
+
+
+
+args = [
+    "netconvert",
+    "--node-files", os.path.join(out_dir, "input_network.nod.xml"),
+    "--edge-files", os.path.join(out_dir, "input_network.edg.xml"),
+    "--connection-files", os.path.join(out_dir, "input_network.con.xml"),
+    "-o", "input_network.net.xml"
+]
 
 scripts = {
     "apply_patch": ["subnetwork.net.xml", "subnetwork_corrected.net.xml", "input_network", "output_network"],
