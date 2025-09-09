d2f5948aee56f7577463fa89a3ced6547da5b327
antoine491000 <antoine.colas@isen-ouest.yncrea.fr>
2025-09-09 16:39:14 +0200

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,20 +1,17 @@
 import subprocess
-import os
 
-out_dir = "data"
-
-
+# Netconvert arguments
 netconvert_args = [
     "netconvert",
-    "--node-files", os.path.join(out_dir, "input_network.nod.xml"),
-    "--edge-files", os.path.join(out_dir, "input_network.edg.xml"),
-    "--connection-files", os.path.join(out_dir, "input_network.con.xml"),
+    "--node-files", "input_network.nod.xml",
+    "--edge-files", "input_network.edg.xml",
+    "--connection-files", "input_network.con.xml",
     "-o", "input_network.net.xml"
 ]
 
 subprocess.run(netconvert_args, check=True)
 
-
+# Apply patch arguments
 apply_patch = [
     "python", "apply_patch.py",
     "-o", "output_network.net.xml",
