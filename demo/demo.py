a4c008ad11e55e13b84da05d0286cc8272d26ed6
antoine491000 <antoine.colas@isen-ouest.yncrea.fr>
2025-09-10 15:37:19 +0200

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,4 +1,24 @@
 import subprocess
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+def taz_to_poly(taz_file: Path, poly_file: Path):
+    tree = ET.parse(taz_file)
+    root = tree.getroot()
+    add_root = ET.Element("additionals", {
+        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "xsi:noNamespaceSchemaLocation": "http://sumo.dlr.de/xsd/additional_file.xsd"
+    })
+    for taz in root.findall("taz"):
+        ET.SubElement(add_root, "poly", {
+            "id": f"taz_{taz.get('id')}",
+            "type": "taz",
+            "color": taz.get("color", "51,128,255"),
+            "fill": "1",
+            "shape": taz.get("shape")
+        })
+    ET.ElementTree(add_root).write(poly_file, encoding="utf-8", xml_declaration=True)
+    print(f"Converted {taz_file} → {poly_file}")
 
 # Netconvert arguments
 netconvert_args = [
@@ -21,3 +41,17 @@ apply_patch = [
 ]
 
 subprocess.run(apply_patch, check=True)
+
+# Convert TAZ to poly
+taz_file = Path("subnetwork.taz.xml")
+poly_file = Path("subnetwork.poly.xml")
+if taz_file.exists():
+    taz_to_poly(taz_file, poly_file)
+
+    subprocess.Popen([
+        "netedit",
+        "--net-file", "output_network.net.xml",
+        "--additional-files", str(poly_file)
+    ])
+else:
+    print("No TAZ detected.")
